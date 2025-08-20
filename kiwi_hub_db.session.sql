4a722417edffcd41e3f42001d5f637adb8066e99
kghent4 <130794676+kghent4@users.noreply.github.com>
2025-08-19 22:04:09 -0500

    change databse page name

--- kiwi_hub_db.session.sql
+++ kiwi_hub_db.session.sql
@@ -1,6 +1,3 @@
-INSERT INTO playlist_queues (page_name, button_name, display_name, position, name, url)
-VALUES
-('apaplanning', 'Button1', 'Playlist 1', 1, 'Song A', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'),
-('apaplanning', 'Button1', 'Playlist 1', 2, 'Song B', 'https://www.youtube.com/watch?v=3JZ_D3ELwOQ'),
-('apaplanning', 'Button2', 'Playlist 2', 1, 'Lecture A', 'https://www.youtube.com/watch?v=2vjPBrBU-TM');
-
+UPDATE playlist_queues
+SET page_name = 'apaplanning'
+WHERE page_name = 'APAPlanning'; -- Or whatever value is mismatched
